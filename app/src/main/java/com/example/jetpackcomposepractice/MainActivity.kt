e4513ff3c488ce42a8a38ffbac7b3ff21db9682d
6879756e <6879756e@gmail.com>
2023-03-12 18:31:13 +0900

    complete: [compose essentials] starter tutorial

--- app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.jetpackcomposepractice
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposepractice.data.SampleData
 import com.example.jetpackcomposepractice.ui.theme.JetpackComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +31,91 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposePracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(message: Message) {
+    Row(modifier = Modifier.padding(8.dp), verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape),
+        )
+        Spacer(
+            modifier = Modifier
+                .width(8.dp)
+                .width(1000.dp)
+        )
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = message.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp),
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor
+            ) {
+                Text(
+                    text = message.body,
+                    modifier = Modifier.padding(4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2,
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    name = "Dark Mode",
+    group = "GROUP",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+@Preview(name = "Light Mode", group = "GROUP", uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Composable
-fun DefaultPreview() {
+fun PreviewMessageCard(message: Message = Message("Ed", "Cowell")) {
     JetpackComposePracticeTheme {
-        Greeting("Android")
+        Surface {
+            MessageCard(message)
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message -> MessageCard(message = message) }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    JetpackComposePracticeTheme {
+        Conversation(SampleData.conversationSample)
     }
 }
