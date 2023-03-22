e4b6b2d477bba05e23857bd2401f3b0c7049dfea
6879756e <6879756e@gmail.com>
2023-03-22 23:58:34 +0900

    complete: [compose essentials] `migration` compose UI in XML

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,13 +16,116 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 
 @Composable
-fun PlantDetailDescription() {
-    Surface {
-        Text("Hello Compose")
+fun PlantDetailDescription(viewModel: PlantDetailViewModel) {
+    val plant by viewModel.plant.observeAsState()
+
+    plant?.let {
+        PlantDetailContent(it)
     }
+
+}
+
+@Composable
+private fun PlantDetailContent(plant: Plant) {
+    Surface {
+        Column(Modifier.padding(dimensionResource(id = R.dimen.margin_normal))) {
+            PlantName(name = plant.name)
+            PlantHeader()
+            PlantWatering(wateringInterval = plant.wateringInterval)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PlantHeaderWithWateringInformation() {
+    Column {
+        PlantHeader()
+        PlantWatering(wateringInterval = 7)
+    }
+}
+
+
+@Composable
+fun PlantHeader(title: String = stringResource(id = R.string.watering_needs_prefix)) {
+    Text(
+        text = title,
+        color = MaterialTheme.colors.primaryVariant,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                start = dimensionResource(id = R.dimen.margin_small),
+                end = dimensionResource(id = R.dimen.margin_small),
+                top = dimensionResource(id = R.dimen.margin_normal)
+
+            )
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
+
+
+@Composable
+fun PlantWatering(wateringInterval: Int) {
+    val quantityString = LocalContext.current.resources.getQuantityString(
+        R.plurals.watering_needs_suffix,
+        wateringInterval, wateringInterval
+    )
+
+    Text(
+        text = quantityString,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
+
+@Preview
+@Composable
+fun PlantDetailContentPreview() {
+    val plant = Plant("id", "Apple", "description", 3, 30, "")
+    MaterialTheme {
+        PlantDetailContent(plant)
+    }
+}
+
+@Composable
+fun PlantName(name: String) {
+    Text(
+        text = name,
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(id = R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PlantNamePreview(name: String = "Apple") {
+    PlantName(name = name)
 }
