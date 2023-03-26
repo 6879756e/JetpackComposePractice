306fbe11413bf6a8024c9d6f17adfd5004bbd301
6879756e <6879756e@gmail.com>
2023-03-26 21:26:09 +0900

    complete: `accessibility` codelab

--- AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/article/ArticleScreen.kt
+++ AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/article/ArticleScreen.kt
@@ -35,8 +35,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetnews.R
 import com.example.jetnews.data.posts.PostsRepository
 import com.example.jetnews.data.posts.impl.post3
 import com.example.jetnews.model.Post
@@ -94,10 +98,12 @@ fun ArticleScreen(
                     )
                 },
                 navigationIcon = {
-                    IconButton(onClick = onBack) {
+                    IconButton(
+                        onClick = onBack
+                    ) {
                         Icon(
                             imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = null
+                            contentDescription = stringResource(id = R.string.cd_navigate_up)
                         )
                     }
                 }
